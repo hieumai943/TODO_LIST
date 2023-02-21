a7e89c9c8a1a3225b5502430e86b2408ace83076
hieumai943 <hieumai09042003@gmail.com>
2023-02-22 00:08:55 +0700

    finish delete item just left date and clear all

--- main.js
+++ main.js
@@ -1,32 +1,37 @@
 const inputVal = document.getElementsByClassName('inputVal')[0];
 const addTaskBtn = document.getElementsByClassName('btn')[0];
 let tasklist;
-addTaskBtn.addEventListener('click', ()=>{
-    let localItems = JSON.parse(localStorage.getItem('localItem'))
-    if(localItems ===null){
-        tasklist =[];
+addTaskBtn.addEventListener('click', () => {
+    if (document.querySelector('.header input').value.length == 0) {
+        alert("Please Enter a Task");
     }
-    else{
-        tasklist = localItems;
+    else {
+        let localItems = JSON.parse(localStorage.getItem('localItem'))
+        if (localItems === null) {
+            tasklist = [];
+        }
+        else {
+            tasklist = localItems;
+        }
+        tasklist.push(inputVal.value);
+        localStorage.setItem('localItem', JSON.stringify(tasklist))
+        showlist();
     }
-   tasklist.push(inputVal.value);
-   localStorage.setItem('localItem', JSON.stringify(tasklist))
-   showlist();
 })
-function showlist(){
-    let outPut =``;
-   let localItems = JSON.parse(localStorage.getItem('localItem'));
-   if(localItems ===null){
-        tasklist =[];
+function showlist() {
+    let outPut = ``;
+    let localItems = JSON.parse(localStorage.getItem('localItem'));
+    if (localItems === null) {
+        tasklist = [];
     }
-    else{
+    else {
         tasklist = localItems;
     }
     tasklist.forEach((data, index) => {
-        outPut += 
-        `<div class="task">
+        outPut +=
+            `<div class="task">
         
-        <span id="taskname">
+        <span id="taskname" onclick="completed(${index})">
             <i class="fa fa-circle" aria-hidden="true"style="font-size:10px;padding-right: 10px;"></i>
           ${tasklist[index]}
             </span>
@@ -37,9 +42,22 @@ function showlist(){
     });
     document.querySelector('.tasks').innerHTML = outPut;
 }
-function deleteItem(index){
-tasklist.splice(index,1)
-localStorage.setItem('localItem', JSON.stringify(tasklist))
 showlist();
+
+function deleteItem(index) {
+    tasklist.splice(index, 1)
+    localStorage.setItem('localItem', JSON.stringify(tasklist))
+    showlist();
 }
 
+function completed(index) {
+    var tasks = document.querySelectorAll('#taskname');
+    tasks[index].classList.toggle('completed');
+
+    setTimeout(function () {
+        tasklist.splice(index, 1)
+        localStorage.setItem('localItem', JSON.stringify(tasklist))
+        showlist();
+    }, 2000)
+
+}
